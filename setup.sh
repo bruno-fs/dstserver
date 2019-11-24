84ee16393c58f04222499b9dc731597c5206437e
Bruno FS <fsouza.bruno@gmail.com>
2019-11-23 21:17:07 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 sudo add-apt-repository multiverse
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install lib32gcc1 steamcmd
+sudo apt install lib32gcc1 libcurl3-gnutls libcurl3-gnutls:i386 steamcmd -y
 
 steamcmd +quit
